b765d11126a0f1417b656bb7cf0b4fe634234ffe
Raül Torralba Adsuara <raul.torralba@artansoft.com>
2017-11-17 18:15:55 +0100

    Added new test to CelsiusAFahrenheit

--- src/test/java/conversor/CelsiusAFahrenheitTest.java
+++ src/test/java/conversor/CelsiusAFahrenheitTest.java
@@ -10,4 +10,10 @@ public class CelsiusAFahrenheitTest {
         ConversorDesdeCelsius conversor = new CelsiusAFahrenheit();
         assertEquals(32.0, conversor.covierteDesdeCelsius(0));
     }
+
+    @Test
+    public void conviertCuarentaCelsiusAFahrenheitTest() {
+        ConversorDesdeCelsius conversor = new CelsiusAFahrenheit();
+        assertEquals(104.0, conversor.covierteDesdeCelsius(40));
+    }
 }
